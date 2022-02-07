55b77f71392fd72ccf65171321ae36feaa7c585e
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-02-07 17:16:58 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".idc-swiper", {
+    // Default parameters
+    loop: true,
+    direction: "horizontal",
+    grabCursor: true,
+
+    navigation: {
+        nextEl: ".idc-swiper-button-next",
+        prevEl: ".idc-swiper-button-prev",
+    },
+});
